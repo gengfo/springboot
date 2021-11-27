f3d7e548a6da531e6214223f82d79325926be4e3
GengFo <gengfo@gmail.com>
2021-11-27 23:32:10 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # springboot
+1. 2021-11-27 add e-book-xlj-springboot-vue
+	https://gitee.com/liuge1988/mango-platform
+	
---
+++ e-book-xlj-springboot-vue/mango-ui/src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="page">
+        <h2>NotFound Page</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
