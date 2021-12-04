585137ae453a5044eebda52c39ae98cc83ce86a1
GengFo <gengfo@gmail.com>
2021-12-04 09:03:15 +0800

    [update]

--- e-book-xlj-springboot-vue/mango-ui/src/router/index.js
+++ e-book-xlj-springboot-vue/mango-ui/src/router/index.js
@@ -12,13 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
+    }, {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
+    }, {
       path: '/404',
       name: 'notFound',
       component: NotFound
